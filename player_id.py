a109c61d4b06673116035d3251be58f7268d3721
aslamdien <aslamdien90@gmail.com>
2021-06-18 20:02:29 +0200

    New Changes

--- player_id.py
+++ player_id.py
@@ -3,6 +3,9 @@ import rsaidnumber
 from datetime import *
 from tkinter import messagebox
 import re
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 root = Tk()
 root.title("Player ID")
@@ -36,7 +39,7 @@ class Player:
         self.id_ent = Entry(master)
         self.id_ent.config(state = "readonly")
         self.id_ent.place(x=200, y=200)
-        self.button1 = Button(master, text = "Verify", command = self.calculate_age ,borderwidth = "3")
+        self.button1 = Button(master, text = "Verify", command = self.valid_age ,borderwidth = "3")
         self.button1.place(x=100, y=300, width = 100)
         self.button1.config(bg="yellow")
 
@@ -48,22 +51,60 @@ class Player:
             self.id_ent.config(state = "readonly")
             self.email_ent.delete(0, END)
 
-    def calculate_age(self):
+    def valid_age(self):
+        player_Id = self.name_ent.get().strip() + self.id_ent.get()[2:4]
+        player = open('details.txt', 'a')
+        player.write("Username: " + self.name_ent.get())
+        player.write('\n')
+        player.write("Email: " + self.email_ent.get())
+        player.write('\n')
+        player.write("Id Number: " + self.id_ent.get())
+        player.write('\n')
+        player.write("Player ID: " + player_Id)
+        player.write('\n')
+        senders_email = "aslamdien90@gmail.com"
+        receivers_email = self.email_ent.get()
+        password = "nitrocharge"
         try:
+            for i in range(len(self.email_ent.get())):
+                if self.email_check():
+                    subject = "Hello Player"
+                    msg = MIMEMultipart()
+                    msg["From"] = senders_email
+                    msg["To"] = receivers_email
+                    msg["Subject"] = subject
+
+                    message = ("This Is Your Player ID: " + str(player_Id))
+                    message = message
+                    msg.attach(MIMEText(message, 'plain'))
+                    text = msg.as_string()
+                    s = smtplib.SMTP("smtp.gmail.com", 587)
+
+                    s.starttls()
+
+                    s.login(senders_email, password)
+                    s.sendmail(senders_email, receivers_email, text)
+                    s.quit()
+                    break
             id_number = rsaidnumber.parse(self.id_ent.get())
             age = str((datetime.today() - id_number.date_of_birth) // timedelta(365.25))
 
             if int(age) >= 18:
                 messagebox.showinfo("Qualification", "Let`s Play")
+                player.close()
+                root.destroy()
             elif int(age) < 18:
                 year = str(int(age) - 18)
                 messagebox.showinfo("Qualification", "Your Are Too Young to Play. Please Try Again In" + year + " Year(s)")
-                messagebox.showerror("ID Error", "ID Must Have 13 Digits Only")
 
         except ValueError:
             if str(self.id_ent.get()) != "":
                 messagebox.showerror("ID ERROR!!!", "Please Enter Valid Integer")
                 self.id_ent.delete(0, END)
+
+            elif len(self.id_ent.get()) < 13:
+                messagebox.showerror("Error", "ID Numbers Only Have 13 Digits")
+                self.id_ent.delete(0,END)
 
             else:
                 messagebox.showerror("ID ERROR!!", "Please Enter ID Number")
